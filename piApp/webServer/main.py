b3b7a21149ae2daa6abed48cd0366ff93daa564d
Racjul <racjul2003@icloud.com>
2023-02-17 12:28:15 -0500

    creation des templates pour le serveur web

--- piApp/webServer/main.py
+++ piApp/webServer/main.py
@@ -1,5 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+
+app = Flask(__name__, static_url_path='/static')
 @app.route('/')
-def hello_world():
-    return 'Hello world!'
+def index():
+    return render_template("index.html", name=name)
