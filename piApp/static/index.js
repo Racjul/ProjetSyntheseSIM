35a970f2a2bf86e51300aaabead3a5744b9e1a69
Racjul <racjul2003@icloud.com>
2023-02-28 10:07:38 -0500

    modification du code HTML/JS/CSS

--- piApp/static/index.js
+++ piApp/static/index.js
@@ -1,4 +1,13 @@
-console.log("test")
+
+document.body.onload = addElement;
+console.log("test");
+function addElement(){
+    for(let i= 1; i<=64;i++){
+        let nouvellecase = document.createElement("div");
+        nouvellecase.id = i.toString();
+        document.getElementById('board').appendChild(nouvelleCase);  
+    }
+}
 var socket = io();
     socket.on('connect', function() {
         socket.emit('my event', {data: 'I\'m connected!'});
