86bcb0c4ec59c8cc21ad3c5f4221a0bbfeb04b12
Racjul <racjul2003@icloud.com>
2023-03-01 12:06:11 -0500

    modification serveur public

--- piApp/app.py
+++ piApp/app.py
@@ -6,22 +6,22 @@ from flask_socketio import SocketIO, send,emit
 
 app = Flask(__name__,static_url_path='/static')
 app.config['SECRET_KEY'] = 'secret!'
-socket= SocketIO(app, async_mode='eventlet')
+socketio= SocketIO(app, async_mode='eventlet')
 
 
 
 @socketio.on('message')
 def handle_message(data):
     print('received message: ' + data)
-    socket.send("test")
+    socketio.send("test")
   
 
 @app.route('/')
 def index():
-    return render_template('index.html', async_mode=socket.async_mode)
+    return render_template('index.html', async_mode=socketio.async_mode)
 
 if __name__ == '__main__':
-    socket.run(app)
+    socketio.run(app)
 
     
     
