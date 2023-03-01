f3742b026d0324a22bc1fd38d2ec37ee0dde9dae
Racjul <racjul2003@icloud.com>
2023-03-01 08:02:42 -0500

    initialisation du projet

---
+++ piApp/main.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask,render_template
+from flask_socketio import SocketIO, send,emit
+
+app = Flask(__name__,static_url_path='/static')
+app.config['SECRET_KEY'] = 'secret!'
+socketio= SocketIO(app)
+   
+@socketio.on('message')
+def handle_message(data):
+    print('received message: ' + data)
+
+    
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    socketio.run(app)
--- piApp/static/index.js
+++ piApp/static/index.js
@@ -1,14 +1,27 @@
 
-document.body.onload = addElement;
-console.log("test");
-function addElement(){
-    for(let i= 1; i<=64;i++){
-        let nouvellecase = document.createElement("div");
-        nouvellecase.id = i.toString();
-        document.getElementById('board').appendChild(nouvelleCase);  
-    }
+
+const board = document.getElementById("board");
+
+for(let i =1; i<=8;i++){
+    let nouvelleColonne = document.createElement("div"); 
+    nouvelleColonne.id = "row_"+i.toString();
+    nouvelleColonne.className= "row"
+    board.appendChild(nouvelleColonne);  
 }
-var socket = io();
+
+
+let cpt = 1;
+for(let i= 1; i<=64;i++){
+    let nouvellecase = document.createElement("div");
+    nouvellecase.id = i.toString();
+    nouvellecase.className= "case"
+    document.getElementById("row"+cpt.toString).appendChild(nouvellecase);
+    if(i%8==0){
+        cpt ++
+    }  
+
+}
+var socket = io.connect('http://' + document.domain + ':' + location.port);
     socket.on('connect', function() {
-        socket.emit('my event', {data: 'I\'m connected!'});
+        console.log(connecté);
     });
