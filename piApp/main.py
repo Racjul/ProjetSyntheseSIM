ffbe7b1e92392b3de8937ff39db227b45c220d8f
Racjul <racjul2003@icloud.com>
2023-03-01 09:42:11 -0500

    test

--- piApp/main.py
+++ piApp/main.py
@@ -6,14 +6,16 @@ app = Flask(__name__,static_url_path='/static')
 app.config['SECRET_KEY'] = 'secret!'
 socketio= SocketIO(app)
    
-@socketio.on('message')
-def handle_message(data):
-    print('received message: ' + data)
 
-    
-@app.route('/')
-def index():
-    return render_template("index.html")
 
 if __name__ == '__main__':
     socketio.run(app)
+
+    
+@socketio.on('message')
+def handle_message(data):
+    print('received message: ' + data)
+  
+@app.route('/')
+def index():
+    return render_template("index.html")
