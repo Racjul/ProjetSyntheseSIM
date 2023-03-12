8524119188f9fd1b4ff46b45124fb58b9989c9d3
Mbaril20 <mathisbaril1@hotmail.com>
2023-03-12 16:46:58 -0400

    test

--- piApp/static/index.js
+++ piApp/static/index.js
@@ -52,30 +52,7 @@ for(let i=1;i<=8;i++)
     for(let j =1;j<=8;j++){
         
         document.getElementById(notation[8-i]+(j).toString()).addEventListener("click", function(e){
-            
-            console.log(document.getElementById(e.target.id).style.backgroundImage);
-            if(document.getElementById(e.target.id).style.backgroundImage!="")
-            {
-                document.getElementById(e.target.id).style.border="thick solid red";
-                caseI=e.target.id;
-                pieceDeplacement= document.getElementById(e.target.id).style.backgroundImage.substring(20,22);
-                console.log(caseI)
-                console.log("");
-                return;
-            }
-
-            if(caseI == null)
-            {
-                console.log("case initiale null");
-                return;
-            }
-
-            console.log("déplacement");
-            ajouterPiece(pieceDeplacement,e.target.id);
-            document.getElementById(caseI).style.backgroundImage=null;
-            document.getElementById(caseI).removeEventListener("click",function(e){});
-            document.getElementById(caseI).style.border="thick solid transparent";
-            caseI=null;
+            deplacer(e);
         },false)
     }
 }
@@ -118,67 +95,45 @@ function start(){
     ajouterPiece("wn","a7");
     ajouterPiece("wr","a8");
    
-    
-    /*document.getElementById("h1").style.backgroundImage = "url('/static/images/br.png')";
-    document.getElementById("h2").style.backgroundImage = "url('/static/images/bn.png')";
-    document.getElementById("h3").style.backgroundImage = "url('/static/images/bb.png')";
-    document.getElementById("h4").style.backgroundImage = "url('/static/images/bk.png')";
-    document.getElementById("h5").style.backgroundImage = "url('/static/images/bq.png')";
-    document.getElementById("h6").style.backgroundImage = "url('/static/images/bb.png')";
-    document.getElementById("h7").style.backgroundImage = "url('/static/images/bn.png')";
-    document.getElementById("h8").style.backgroundImage = "url('/static/images/br.png')";
-    */
-//création des pièces blanches
-    /*document.getElementById("a1").style.backgroundImage = "url('/static/images/wr.png')";
-    document.getElementById("a2").style.backgroundImage = "url('/static/images/wn.png')";
-    document.getElementById("a3").style.backgroundImage = "url('/static/images/wb.png')";
-    document.getElementById("a4").style.backgroundImage = "url('/static/images/wk.png')";
-    document.getElementById("a5").style.backgroundImage = "url('/static/images/wq.png')";
-    document.getElementById("a6").style.backgroundImage = "url('/static/images/wb.png')";
-    document.getElementById("a7").style.backgroundImage = "url('/static/images/wn.png')";
-    document.getElementById("a8").style.backgroundImage = "url('/static/images/wr.png')";*/
 }
 
 function ajouterPiece(piece,location){
     element = document.getElementById(location)
     element.style.backgroundImage = "url('/static/images/"+piece+".png')"
-
-    element.addEventListener("click",function(e)
-    {
-        /*document.getElementById(e.target.id).style.border="thick solid red";
-        caseI=e.target.id;
-        pieceDeplacement= piece;
-        console.log(caseI)
-        */
-    },false)
-
 }
 
 
 
-/*
-function deplacer(caseF){
+
+function deplacer(e){
+    console.log(document.getElementById(e.target.id).style.backgroundImage);
+            if(document.getElementById(e.target.id).style.backgroundImage!="")
+            {
+                document.getElementById(e.target.id).style.border="thick solid red";
+                caseI=e.target.id;
+                pieceDeplacement= document.getElementById(e.target.id).style.backgroundImage.substring(20,22);
+                console.log(caseI)
+                console.log("");
+                return;
+            }
+
+            if(caseI == null)
+            {
+                console.log("case initiale null");
+                return;
+            }
+
+            console.log("déplacement");
+            ajouterPiece(pieceDeplacement,e.target.id);
+            document.getElementById(caseI).style.backgroundImage=null;
+            document.getElementById(caseI).removeEventListener("click",function(e){});
+            document.getElementById(caseI).style.border="thick solid transparent";
+            caseI=null;
     
-    console.log("case finale changé");
-
-    if(caseI == null)
-    {
-        console.log("case initiale null");
-         return;
-    }
-
-    if(document.getElementById(caseI).style.backgroundImage!=null)
-    {
-        
-        ajouterPiece(pieceDeplacement,caseF);
-        document.getElementById(caseI).style.backgroundImage=null;
-        document.getElementById(caseI).removeEventListener("click",function(e){});
-
-    }
 
 
 }
-*/
+
 
 
 
