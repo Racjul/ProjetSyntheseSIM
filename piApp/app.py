59ad862ea4554d60f1de59a8dc8c9eabdeb0c10e
Racjul <racjul2003@icloud.com>
2023-03-22 11:38:47 -0400

    test

--- piApp/app.py
+++ piApp/app.py
@@ -17,7 +17,7 @@ app.config['SECRET_KEY'] = 'test'
 socketio= SocketIO(app, async_mode='eventlet')
 
 #permet de donner la directory de l'engine d'échec
-stockfish = Stockfish(path="/home/admin/ProjetSyntheseSIM/stockfish-10-linux/Linux/stockfish_10_x64_bmi2")
+stockfish = Stockfish(path="/usr/games/stockfish")
 
 #permet de print dans la console, les messages reçus provenant du site web
 @socketio.on('message')
