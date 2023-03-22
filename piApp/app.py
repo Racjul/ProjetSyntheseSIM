969ae1aa343bb97c09b626dc21aaf9f26fb9b0f0
Mbaril20 <mathisbaril1@hotmail.com>
2023-03-22 09:52:47 -0400

    test

--- piApp/app.py
+++ piApp/app.py
@@ -32,7 +32,7 @@ def handle_my_custom_event(piece,id ,caseInitial):
         stockfish.make_moves_from_current_position([caseInitial+id])
         socketio.emit("coupValide",str(piece)+ str(id)+str(caseInitial))
         print(stockfish.get_board_visual())
-        stockfish.get_evaluation()
+        print(stockfish.get_top_moves(3))
         
     else:
         socketio.emit("coupInvalide",caseInitial)
