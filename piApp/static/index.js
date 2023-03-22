e26867209e96b68b36446000ccfaa0e39f0cf281
Mbaril20 <mathisbaril1@hotmail.com>
2023-03-22 18:41:41 -0400

    manger en passant

--- piApp/static/index.js
+++ piApp/static/index.js
@@ -48,7 +48,7 @@ socket.on("coupValide",(info)=>
     console.log("piece deplacement: " + pieceDeplacement);
     console.log("caseI sub: " + caseI.substring(0,1) );
     console.log("caseF sub: " + caseF.substring(0,1) );
-    console.log("background: " + document.getElementById(caseF).style.backgroundImage)
+    console.log("background: " + document.getElementById(caseF).style.backgroundImage == " ")
 
     if(pieceDeplacement=="bp")
     {
