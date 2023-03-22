0028ca9c027a147d42d65981997094ca58af8049
Mbaril20 <mathisbaril1@hotmail.com>
2023-03-22 09:59:08 -0400

    test

--- piApp/app.py
+++ piApp/app.py
@@ -32,8 +32,9 @@ def handle_my_custom_event(piece,id ,caseInitial):
         stockfish.make_moves_from_current_position([caseInitial+id])
         socketio.emit("coupValide",str(piece)+ str(id)+str(caseInitial))
         
-        print(stockfish.does_current_engine_version_have_wdl_option())
+        #print(stockfish.does_current_engine_version_have_wdl_option())
 
+        print(stockfish.get_best_move())
         print(stockfish.get_board_visual())
         
     else:
