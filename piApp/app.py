b764f96ee0d24e76a6e82aefe7951c0311d53ee9
Mbaril20 <mathisbaril1@hotmail.com>
2023-03-22 09:59:49 -0400

    test

--- piApp/app.py
+++ piApp/app.py
@@ -35,7 +35,7 @@ def handle_my_custom_event(piece,id ,caseInitial):
         #print(stockfish.does_current_engine_version_have_wdl_option())
 
         print(stockfish.get_best_move())
-        print(stockfish.get_board_visual())
+        #print(stockfish.get_board_visual())
         
     else:
         socketio.emit("coupInvalide",caseInitial)
