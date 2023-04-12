6f19517794a894ae33dc76662056e3b7107a6821
Mbari20 <mathisbaril1@hotmail.com>
2023-04-12 10:30:11 -0400

    Modification du script scara

--- Arduino/Scara/Scara.ino
+++ Arduino/Scara/Scara.ino
@@ -1,119 +1,78 @@
 
-/*
- Stepper Motor Control - one revolution
-
- This program drives a unipolar or bipolar stepper motor.
- The motor is attached to digital pins 8 - 11 of the Arduino.
-
- The motor should revolve one revolution in one direction, then
- one revolution in the other direction.
+#include <MultiStepper.h>
+#include <AccelStepper.h>
 
 
- Created 11 Mar. 2007
- Modified 30 Nov. 2009
- by Tom Igoe
+AccelStepper stepperBottom(AccelStepper::FULL4WIRE, 4, 5, 6, 7);
+AccelStepper stepperTop(AccelStepper::FULL4WIRE, 8,9,10,11);
 
- */
-
-#include <Stepper.h>
-
-const int stepsPerRevolutionBottom = 200;  // change this to fit the number of steps per revolution
-const int stepsPerRevolutionUpper = 336;  // change this to fit the number of steps per revolution
-const int buttonPin = 2;
-// for your motor
-
-// initialize the stepper library on pins 8 through 11:
-Stepper myStepperBottom(stepsPerRevolutionBottom, 8, 9, 10, 11);
-Stepper myStepperUpper(stepsPerRevolutionUpper, 4, 5, 6, 7);
-
-
-
-
+MultiStepper steppers;
+eval "$(ssh-ag
 int boutonPhase = LOW;
+const int buttonPin = 2;
+long position[2];
+
+void deplacer(double,double,int,int);
+ 
 
 void setup() {
-  // set the speed at 60 rpm:
-  myStepperBottom.setSpeed(30);
-  myStepperUpper.setSpeed(30);
+  // put your setup code here, to run once:
 
-    // initialize the pushbutton pin as an input:
-  pinMode(buttonPin, INPUT);
-  
-  // initialize the serial port:
   Serial.begin(9600);
+
+
+
+  steppers.addStepper(stepperTop);
+  steppers.addStepper(stepperBottom);
+
+
+ 
 }
 
 void loop() {
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPU+GhHJRDMvnOdKu9NSlo2q6xJ7pWS1pxcwx4lQ2MpX mathisbaril1@hotmail.com
+
+  boutonPhase = digitalRead(buttonPin);
 
   if(boutonPhase == HIGH)
   {
-    deplacer(90,90);
-    boutonPhase = LOW;
-
-    delay(2000);
-  }
-  else
-  {
-    boutonPhase = digitalRead(buttonPin);
-  }
-
-
-}
-
-
-int angleToStep(float angle, bool upper)
-{
-  return ceil((angle * 331)/360);
-}
-
-void deplacer(float angleUp, float angleBottom)
-{
-
-  float stepUp = angleToStep(angleUp,true);
-  float stepBottom = angleToStep(angleBottom,false);
-
-  int i =0;
-  int cptStepBottom=0;
-  int cptStepUp=0;
-
-  while(i<=abs(stepUp) || i<= abs(stepBottom))
-  {
-    if(i<=abs(stepUp))
-    {
-      if(stepUp<0)
-      {
-        myStepperUpper.step(-1);
-      }
-      else if(stepUp>0)
-      {
-        myStepperUpper.step(1);
-      }
-    }
     
-    if(i<=abs(stepBottom))
-    {
-      if(stepBottom<0)
-      {
-        myStepperBottom.step(-1);
-      }
-      else if(stepBottom>0)
-      {
-        myStepperBottom.step(1);
-      }
-    }
-    i++;
+    
+    deplacer(180,90,200,1000);
+    deplacer(0,180,100,0);
+    deplacer(-180,270,100,1000);
+    deplacer(0,0,150,2000);
+
+    deplacer(180,45,200,1000);
+    deplacer(0,135,100,0);
+    deplacer(-180,225,100,1000);
+    deplacer(0,0,150,2000);
+
+
+    deplacer(180,45,200,1000);
+    deplacer(135,90,50,0);
+    deplacer(180,135,50,1000);
+    deplacer(0,0,150,2000);
+    
+
+ 
   }
-
-
-
 
 }
 
 
+void deplacer(double angleTop, double angleBottom, int maxSpeed, int delayTime)
+{
+  stepperBottom.setMaxSpeed(maxSpeed);
+  stepperTop.setMaxSpeed(maxSpeed);
 
 
+  position[1] = ceil(angleTop * 333 / 360);
+  position[0] = ceil(angleBottom * 333 / 360);
 
+  steppers.moveTo(position);
+  steppers.runSpeedToPosition();
 
-
-
+  delay(delayTime);
+}  
 
