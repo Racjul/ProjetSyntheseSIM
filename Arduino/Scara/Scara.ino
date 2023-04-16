e05790f194bdd2bfbab84cf1135ef606b7084e40
Mbari20 <mathisbaril1@hotmail.com>
2023-04-16 14:11:07 -0400

    algo et raspberry pi

--- Arduino/Scara/Scara.ino
+++ Arduino/Scara/Scara.ino
@@ -1,59 +1,97 @@
 
 #include <MultiStepper.h>
 #include <AccelStepper.h>
+#include <string.h>
 
 
 AccelStepper stepperBottom(AccelStepper::FULL4WIRE, 4, 5, 6, 7);
 AccelStepper stepperTop(AccelStepper::FULL4WIRE, 8,9,10,11);
 
 MultiStepper steppers;
-int boutonPhase = LOW;
-const int buttonPin = 2;
 long position[2];
 
 void deplacer(double,double,int,int);
- 
+  int degTopI =0;
+  int degBottomI = 0;
+  
+  int degTopF = 0;
+  int degBottomF = 0;
+  String angles = "";
 
 void setup() {
+  // put your setup code here, to run once:
 
   Serial.begin(9600);
 
-
+  stepperBottom.setAcceleration(20);
+  stepperTop.setAcceleration(20);
 
   steppers.addStepper(stepperTop);
   steppers.addStepper(stepperBottom);
-
 
  
 }
 
 void loop() {
-
-  boutonPhase = digitalRead(buttonPin);
-
-  if(boutonPhase == HIGH)
+  
+  
+  if(Serial.available())
   {
+    angles = Serial.readStringUntil("\n");
+  }
+
+  
+  if(angles != "")
+  {
+    delay(1000);
     
     
-    deplacer(180,90,200,1000);
-    deplacer(0,180,100,0);
-    deplacer(-180,270,100,1000);
-    deplacer(0,0,150,2000);
 
-    deplacer(180,45,200,1000);
-    deplacer(0,135,100,0);
-    deplacer(-180,225,100,1000);
-    deplacer(0,0,150,2000);
+    
+    int upI = angles.substring(0,angles.indexOf(",")).toInt();
+    angles.remove(0,angles.indexOf(",")+1);
+    int bottomI = angles.substring(0,angles.indexOf(",")).toInt();
+    angles.remove(0,angles.indexOf(",")+1);
+    //Serial.println(upI);
+    //erial.println(bottomI);
+    deplacer(upI,bottomI,150,1000);
 
+    int tempUp;
+    int tempBottom;
+    while(angles.length()>0)
+    {
+      tempUp = angles.substring(0,angles.indexOf(",")).toInt();
+      angles.remove(0,angles.indexOf(",")+1);
+      tempBottom = angles.substring(0,angles.indexOf(",")).toInt();
+      angles.remove(0,angles.indexOf(",")+1);
+      //Serial.println(tempUp);
+      //Serial.println(tempBottom);
+      deplacer(tempUp,tempBottom,75,0);
+    }
+  
+    delay(1000);
 
-    deplacer(180,45,200,1000);
-    deplacer(135,90,50,0);
-    deplacer(180,135,50,1000);
-    deplacer(0,0,150,2000);
+    deplacer(0,0,150,0);
+
+  
+    
+    
+    
+    angles = "";
     
 
  
   }
+
+
+  
+
+
+
+
+
+
+
 
 }
 
@@ -64,8 +102,8 @@ void deplacer(double angleTop, double angleBottom, int maxSpeed, int delayTime)
   stepperTop.setMaxSpeed(maxSpeed);
 
 
-  position[1] = ceil(angleTop * 333 / 360);
-  position[0] = ceil(angleBottom * 333 / 360);
+  position[1] = round(angleTop * 333 / 360);
+  position[0] = round(angleBottom * 333 / 360);
 
   steppers.moveTo(position);
   steppers.runSpeedToPosition();
--- piApp/algo.py
+++ piApp/algo.py
@@ -1,17 +1,247 @@
-import time
+import math
 import serial
-import threading
 
-def connection():
-    con = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-    con.reset_input_buffer()
-    read(con)
-def read(con):
-    con.write(b"Hello from Pi!\n")
-    line = con.readline().decode('utf-8').rstrip()
-    print(line)
-    timer  = threading.Timer(1,read(con)).start()
-if __name__ == '__main__':
-    connection()
 
-     
+##arduino = serial.Serial('/dev/ttyACM5', 9600, timeout=.1)
+
+## Variables globales
+listNotation = ['a','b','c','d','e','f','g',]
+
+def nbCaseTraverser(caseI, caseF):
+    listMvt = [caseF]
+
+    if caseI[0] == caseF[0] or caseI[1] == caseF[1]:
+        return listMvt
+
+    ## si c'est en diagonale on continue
+    ## 4 possibilitées
+
+    diffNotation = listNotation.index(caseF[0]) - listNotation.index(caseI[0])
+    diffNum = int(caseF[1]) - int(caseI[1])
+
+    if diffNotation > 0 and diffNum > 0:
+        num = int(caseI[1])
+        for x in range(listNotation.index(caseI[0]) , listNotation.index(caseF[0])):
+            print(x)
+            num +=1
+            temp = str(listNotation[x+1]) + str(num) 
+            listMvt.append(temp)
+        listMvt.pop(0)
+        return listMvt
+
+    if diffNotation < 0 and diffNum > 0:
+        i=1
+        num = int(caseI[1])
+        for x in range(listNotation.index(caseF[0]) , listNotation.index(caseI[0])):
+            print(x)
+            num +=1
+            temp = str(listNotation[listNotation.index(caseI[0])-i]) + str(num) 
+            listMvt.append(temp)
+            i+=1
+        listMvt.pop(0)
+        return listMvt
+    
+
+    if diffNotation < 0 and diffNum < 0:
+        i=1
+        num = int(caseI[1])
+        for x in range(listNotation.index(caseF[0]) , listNotation.index(caseI[0])):
+            print(x)
+            num -=1
+            temp = str(listNotation[listNotation.index(caseI[0])-i]) + str(num) 
+            listMvt.append(temp)
+            i+=1
+        listMvt.pop(0)
+        return listMvt
+    
+    if diffNotation > 0 and diffNum < 0:
+        i=1
+        num = int(caseI[1])
+        for x in range(listNotation.index(caseI[0]) , listNotation.index(caseF[0])):
+            print(x)
+            num -=1
+            temp = str(listNotation[x+1]) + str(num) 
+            listMvt.append(temp)
+            i+=1
+        listMvt.pop(0)
+        return listMvt
+
+
+  
+
+
+
+def grille():
+    posX = -111
+    posY = -111
+    
+    grille = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
+              
+              
+
+    i=0
+    j=0
+    k=0
+    
+    
+    for x in range(17):
+        for y in range(17):
+            if k % 2 ==0 and y % 2 ==0:
+                case = {
+                "notation" : listNotation[i] + str(y+1),
+                "posX" : posX,
+                "posY" : posY + (16 * y)
+                }
+                i+=1
+            else :
+                case = {
+                "notation" : 'ligne',
+                "posX" : posX,
+                "posY" : posY + (16 * y)
+                }
+
+            grille[7-y][k] = case
+        if k % 2 ==0:
+            j+=1
+        posX += 16
+        k+=1
+        
+
+    print(grille)
+    return grille
+            
+
+    
+
+
+
+def cooToAngle(x,y,L):
+
+    ### formule de theta2
+    theta2 = math.degrees(2 * math.asin(math.hypot(abs(x),abs(y))/(2 * L)))
+    theta2 = round(theta2,2)
+
+    ##formule de beta
+    b = math.degrees(math.asin(abs(y)/math.hypot(abs(x),abs(y))))
+    b = round(b,2)
+
+    ## formule d'alpha
+    a = (180 - theta2) / 2
+
+    ## theta 1
+    theta1 = b + a
+    
+
+    ##############################################################
+    ## ajustement des cadrant et choix du theta 1
+    # on ajoute 90 degrés au theta 1 par cadrant 
+    # MAIS, on ne veut pas que le theta 1 dépasse 360 degrées, donc on les choisis en fonction de ce paramètre 
+    # éventuellement, on pourra le choisir en fonction du trajet qu'il devra faire pour éviter qu'il dépasse de 360 degrés
+    # (il ne peut pas dépasser 360 degrés à cause d'une contrainte de construction (fils))
+
+    #numérotation des cadrants selon le contexte:  2 1
+    #                                              3 4
+
+    
+    
+    
+    #si on est dans le quadrant 1, on n'a rien a faire
+
+    #s'il est dans le cadrant 2, on ajoute 90 deg au theta 1 (ne peut pas dépasser)
+    if x <= 0 and y >= 0:
+        theta1 = (360 - (2 * theta2))/2 + 180 - theta1
+    
+    
+    #s'il est dans le cadrant 3, on ajoute 180 deg au theta 1 (ne peut pas dépasser)
+    if x < 0 and y < 0:
+        theta1 = (360 - (2 * theta1))/2 + (2*theta1)
+
+    
+    #s'il est dans le cadrant 4, on ajoute 270 deg au theta 1
+    if x >= 0 and y <= 0:
+        theta1 = (360 - (2 * theta2))/2 + 180 - theta1 + 180
+
+
+       
+    
+    if theta1 == 360:
+        theta1 -= 360
+
+    theta1 = round(theta1)
+    theta2 = round(theta2)
+
+    
+    return str(theta2) + ',' + str(theta1)
+
+
+
+
+
+
+
+
+####################################
+# Main()
+
+grille = grille()
+
+xi=0
+xf=0
+yi=0
+yf=0
+L=107
+
+while(True):
+    
+
+    caseI = input("choisissez une case initiale : ")
+    caseF = input("choisissez une case finale: ")
+
+
+
+
+    ## La variable chemin contient toute les cases à partir de la case initiales jusqu'a la case finale 
+    chemin = nbCaseTraverser(caseI,caseF)
+    chemin.insert(0,caseI)
+    print(chemin)
+
+    listCoo = ''
+
+    for x in chemin:
+        for y in range(8):
+            for z in range(8):
+                temp = grille[y][z]
+                if temp.get("notation") == x:
+                    x = temp.get("posX")
+                    y = temp.get("posY")
+                    print(str(x) + ", " + str(y))
+                    listCoo = listCoo + (cooToAngle(x,y,L) + ',')
+                    break
+
+
+    ##arduino.write(str.encode(listCoo))
+        
+    print(listCoo)
+
+
+
+
+
+
+
