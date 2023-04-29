0745f22425305ffee89ec078f5e290e15486e89c
Racjul <racjul2003@icloud.com>
2023-04-29 13:00:27 -0400

    Ajout code multiplexer

--- Arduino/hallEffectsSensorReader.ino
+++ Arduino/hallEffectsSensorReader.ino
@@ -1,12 +1,16 @@
-#include<string.h>
-
+#include<Multiplexer.h>
 void setup()
 {
+    Multiplexer multiplexer1(1,2,3,4,0);
+    Multiplexer multiplexer2(1,2,3,4,1);
+    Multiplexer multiplexer3(1,2,3,4,2);
+    Multiplexer multiplexer4(1,2,3,4,3);
     Serial.begin(9600);
 }
 
 void loop()
 {
+
     if (Serial.available() > 0) {
        
         Serial.print("You sent me: ");
